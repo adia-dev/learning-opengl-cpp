c65d763f825c4e1b9d7812dc02bc4beaba00b2e5
🦀 adia-dev <abdoudu78130@gmail.com>
2024-03-10 18:08:00 +0100

    🎨 Setup of shaders and EBO

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 #include <cstddef>
 #include <cstdio>
 #include <glad/glad.h>
+#include <iostream>
+
+const char *vertex_shader_source = "#version 330 core\n"
+                                   "layout (location = 0) in vec3 aPos;\n"
+                                   " void main()\n"
+                                   " {\n"
+                                   " gl_Position = vec4(aPos.xyz, 1.0f);\n"
+                                   " }\n";
+
+const char *fragment_shader_source =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    " void main()\n"
+    " {\n"
+    " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    " }\n";
 
 void c_speed(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -18,8 +34,8 @@ int main(int argc, char **argv) {
   glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GL_FALSE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
-                                        "Fractalllllllls", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL", NULL, NULL);
   if (window == NULL) {
     printf("Failed to create GLFW Window.\n");
     glfwTerminate();
@@ -38,23 +54,74 @@ int main(int argc, char **argv) {
   glfwSetFramebufferSizeCallback(window, c_speed);
 
   // just drawing a square (two triangles) on the whole screen
-  const float vertices[] = {-1.0f, 1.0f, 1.0f, 1.0f,  -1.0f, -1.0f,
-                            1.0f,  1.0f, 1.0f, -1.0f, -1.0f, -1.0f};
+  const float vertices[] = {
+      -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
+  };
 
-  unsigned int VAO, VBO;
+  const unsigned int indices[] = {0, 1, 2, 1, 2, 3};
+
+  unsigned int VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the VBO
   glBindVertexArray(0);             // Unbind the VAO
+
+  unsigned int vertex_shader;
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+  glCompileShader(vertex_shader);
+
+  int success;
+  char info_log[512];
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << info_log << std::endl;
+  }
+
+  unsigned int fragment_shader;
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+  glCompileShader(fragment_shader);
+
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << info_log << std::endl;
+  }
+
+  unsigned int shader_program;
+  shader_program = glCreateProgram();
+  glAttachShader(shader_program, vertex_shader);
+  glAttachShader(shader_program, fragment_shader);
+  glLinkProgram(shader_program);
+
+  glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+    std::cout << "ERROR::PROGRAM::SHADER_PROGRAM::LINKING_FAILED\n"
+              << info_log << std::endl;
+  }
+
+  glUseProgram(shader_program);
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
 
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
@@ -63,8 +130,10 @@ int main(int argc, char **argv) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBindVertexArray(VAO);           // Re-bind the VAO
-    glDrawArrays(GL_TRIANGLES, 0, 6); // Draw 6 vertices (2 triangles)
+    glUseProgram(shader_program); // Use the shader program
+    glBindVertexArray(VAO);       // Re-bind the VAO
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0); // Unbind the VAO
 
     // Check and Events + Buf Swapping
     glfwSwapBuffers(window);
